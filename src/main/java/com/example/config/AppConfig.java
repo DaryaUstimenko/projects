06311589355636705d97bbe6c1d9da502bead947
DaryaUstimenko <145802118+DaryaUstimenko@users.noreply.github.com>
2024-03-18 16:26:39 +0300

    rm

--- src/main/java/com/example/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.config;
-
-import org.springframework.context.annotation.*;
-
-@ComponentScan("com.example")
-@Configuration
-@PropertySource("classpath:application.yaml")
-public class AppConfig {
-}
--- src/main/java/com/example/profiles/Contact.java
+++
@@ -1,8 +0,0 @@
-package com.example.profiles;
-
-import com.example.model.User;
-import java.util.Map;
-
-public interface Contact {
-    Map<String, User> initContacts();
-}
